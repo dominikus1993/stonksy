744c80b85d8ee41e230c33f4e474f0e637c2a940
dominikus1993 <dominikus1910@gmail.com>
2021-02-11 20:17:30 +0100

    csv

---
+++ src/Stonksy.Infrastructure/Dtos/CompanyDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Stonksy.Infrastructure.Dtos
+{
+    public record CompanyDto(string Symbol, string Name, string Market);
+}
--- src/Stonksy.Infrastructure/Providers/Companies/ApiCompaniesProvider.cs
+++ src/Stonksy.Infrastructure/Providers/Companies/ApiCompaniesProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading;
 using Stonksy.Core.Model;
 using Stonksy.Core.Providers.Companies;
@@ -7,6 +8,15 @@ namespace Stonksy.Infrastructure.Providers.Companies
 {
     internal class ApiCompaniesProvider : ICompaniesProvider
     {
+        public const string CompaniesApi = nameof(ApiCompaniesProvider);
+        private HttpClient _client;
+
+        public ApiCompaniesProvider(IHttpClientFactory factory)
+        {
+            _client = factory.CreateClient(CompaniesApi);
+        }
+        
+        
         public IAsyncEnumerable<Company> ProvideCompanies(CancellationToken cancellationToken)
         {
             throw new System.NotImplementedException();
