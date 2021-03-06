cba4291f05f423c9e033c5859e2f95fd50bc9e31
dominikus1993 <dominikus1910@gmail.com>
2021-03-06 17:19:37 +0000

    small changes

--- src/Stonksy.Cli/Program.fs
+++ src/Stonksy.Cli/Program.fs
@@ -1,16 +1,32 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
-
+open FSharp.Control
+open System.Threading
 // Define a function to construct a message to print
 let from whom =
     sprintf "from %s" whom
 
 let test i =
-    as
+    async {
+        printfn $"Start i = {i}, {DateTime.Now}\n"
+        do! Async.Sleep(10000 + (i * 100))
+        let id = Thread.CurrentThread.ManagedThreadId;
+        printfn $"End i = {i}, {DateTime.Now} -> {id}"
+        return i;
+    }
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    do ThreadPool.SetMinThreads(10, 100) |> ignore
+    do ThreadPool.SetMaxThreads(50, 100) |> ignore
+    let thrads = ThreadPool.ThreadCount
+    printfn $"Threads {thrads}"
+    let message = [1..10] 
+                    |> List.rev 
+                    |> AsyncSeq.ofSeq
+                    |> AsyncSeq.mapAsyncParallel(test)
+                    |> AsyncSeq.toListAsync
+                    |> Async.RunSynchronously
+    printfn "Hello world %A" message
     0 // return an integer exit code
