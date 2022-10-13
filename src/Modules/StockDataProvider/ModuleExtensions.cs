d35753b8fc9a44d5e0ab8c698cc84112eab94be5
Dominik.Kotecki <dominikus1910@gmail.com>
2022-10-13 19:59:46 +0200

    small changes

--- src/Modules/StockDataProvider/ModuleExtensions.cs
+++ src/Modules/StockDataProvider/ModuleExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -5,7 +6,7 @@ namespace StockDataProvider;
 
 public static class ModuleExtensions
 {
-    public static IServiceCollection AddStockDataProviderModule(this IServiceCollection services)
+    public static IServiceCollection AddStockDataProviderModule(this IServiceCollection services, IConfiguration configuration)
     {
         return services;
     }
